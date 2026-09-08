aeca659c3d3600f0bd526a4cec320addad205be4
Evan Porter <evanwporter@gmail.com>
2026-09-07 20:47:33 -0700

    OSC 9 notifications support
    remove patch file

--- win.h
+++ win.h
@@ -33,6 +33,7 @@ int xsetcolorname(int, const char *);
 int xgetcolor(int, unsigned char *, unsigned char *, unsigned char *);
 void xseticontitle(char *);
 void xsettitle(char *);
+void xnotify(const char *);
 int xsetcursor(int);
 void xsetmode(int, unsigned int);
 void xsetpointermotion(int);

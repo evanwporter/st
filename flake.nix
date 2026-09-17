e2a5a5e5f2efef714a4c1a929e02bfcdef085cac
Evan Porter <evanwporter@gmail.com>
2026-09-17 07:27:27 -0700

    update flake.nix with program name

--- flake.nix
+++ flake.nix
@@ -1,56 +1,87 @@
 {
-	description = "st-flexipatch development workspace";
+	description = "st development workspace";
 
 	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	inputs.flake-utils.url = "github:numtide/flake-utils";
 
 	outputs = {
 		self,
 		nixpkgs,
-	}: let
-		supportedSystems = ["x86_64-linux" "aarch64-linux"];
-		forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-	in {
-		packages =
-			forAllSystems (system: let
-					pkgs = nixpkgs.legacyPackages.${system};
-				in {
-					default =
-						pkgs.stdenv.mkDerivation {
-							pname = "st-flexipatch";
-							version = "0.9.3";
-							src = self;
+		flake-utils,
+	}:
+		flake-utils.lib.eachDefaultSystem (
+			system: let
+				pkgs = nixpkgs.legacyPackages.${system};
 
-							nativeBuildInputs = [pkgs.pkg-config];
-							buildInputs = with pkgs; [
-								fontconfig
-								freetype
-								harfbuzz
-								libx11
-								libXcursor
-								libxft
-								libxinerama
-								libnotify
-							];
+				st =
+					pkgs.stdenv.mkDerivation {
+						pname = "st";
+						version = "0.9.3";
 
-							installFlags = ["PREFIX=$(out)"];
+						src = self;
+
+						strictDeps = true;
+
+						makeFlags = [
+							"PKG_CONFIG=${pkgs.stdenv.cc.targetPrefix}pkg-config"
+						];
+
+						nativeBuildInputs = with pkgs; [
+							pkg-config
+							ncurses
+						];
+
+						buildInputs = with pkgs; [
+							fontconfig
+							freetype
+							harfbuzz
+							libx11
+							libXcursor
+							libxft
+							libxinerama
+							libnotify
+						];
+
+						preInstall = ''
+							export TERMINFO=$out/share/terminfo
+						'';
+
+						installFlags = [
+							"PREFIX=$(out)"
+						];
+
+						meta = {
+							mainProgram = "st";
 						};
-				});
+					};
+			in {
+				packages = {
+					inherit st;
+					default = st;
+				};
 
-		devShells =
-			forAllSystems (system: let
-					pkgs = nixpkgs.legacyPackages.${system};
-				in {
-					default =
-						pkgs.mkShell {
-							inputsFrom = [self.packages.${system}.default];
-							packages = with pkgs; [
-								bear
-								clang-tools
-								gnumake
-								jq
-								pkg-config
-							];
+				apps = {
+					st =
+						flake-utils.lib.mkApp {
+							drv = st;
+							exePath = "/bin/st";
 						};
-				});
-	};
+				};
+
+				devShells.default =
+					pkgs.mkShell {
+						inputsFrom = [
+							st
+						];
+
+						packages = with pkgs; [
+							bear
+							clang-tools
+							gnumake
+							jq
+							pkg-config
+						];
+					};
+			}
+		);
 }
